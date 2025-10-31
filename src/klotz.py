006e6f083b62c270f1e93056ee02f6024908d686
Cindy <73001331+weng0@users.noreply.github.com>
2025-10-31 19:16:00 +0100

    Add class Cluster which initialized object is composed of 4 Klotz objects, add a draw function that draw the cluster, add setForm() that will give the Cluster a certain shape, for example Z,L,I,T,O.

--- src/klotz.py
+++ src/klotz.py
@@ -25,4 +25,26 @@ class Klotz:
         for i in range(len(self.rows)):
             line = self.rows[i]
             stdscr_fn.addstr(self.y_pos+i, self.x_pos, line)
-        #return lines
+
+import random
+random.seed()
+
+class Cluster:
+    def __init__(self):
+        self.klotz_cluster : Klotz = [Klotz(),Klotz(),Klotz(),Klotz()]
+        self.y = 15
+        self.x = 50
+
+    def setForm(self):
+        y = self.y
+        x = self.x
+        z = [(x,y),(x+6,y),(x+6,y+3), (x+(6)*2, y+3)]
+        for i in range(len(self.klotz_cluster)):
+            k = self.klotz_cluster[i]
+            x, y = z[i]
+            k.setDrawPosition(y,x)
+
+    def drawCluster(self, stdscr_fn):
+        for i in self.klotz_cluster:
+            i.draw(stdscr_fn)
+        
