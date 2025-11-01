81e31aefaee35aaec8d929f57cdb9ef58dde54ea
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-01 15:12:08 +0100

    Make blocks smaller which means less rows and columns and correct some functions. Add enums, add more cluster forms to choose from and adapt another function to this change.

--- src/klotz.py
+++ src/klotz.py
@@ -2,13 +2,13 @@ class Klotz:
     def __init__(self):
         self.x_pos = 0
         self.y_pos = 0
-        self.rows = ['######', '######', '######']
+        self.rows = ['###','###']
 
     def getUnterseite(self):
-        return self.y_pos+2
+        return self.y_pos+1
     
     def get_R_Seite(self):
-        return self.x_pos+5
+        return self.x_pos+2
     
     def get_L_Seite(self):
         return self.x_pos
@@ -25,25 +25,69 @@ class Klotz:
             line = self.rows[i]
             stdscr_fn.addstr(self.y_pos+i, self.x_pos, line)
 
+import enum
+class Formen(enum.Enum):
+    Z = 1
+    L = 2
+    I = 3
+    T = 4
+    O = 5
+
+'''
+T:
+         (x,y)
+(x-3,y+2)(x,y+2)(x+3, y+2)
+
+I:
+(x,y)
+(x,y+2)
+(x,(y+2)*2)
+(x,(y+2)*3)
+
+L:
+(x,y)
+(x,y+2)
+(x,(y+2)*2)(x+3,(y+2)*2)
+
+O:
+(x,y)(x+3, y)
+(x,y+2)(x+3, y+2)
+'''
+
 import random
 random.seed()
-
+'https://www.geeksforgeeks.org/python/enum-in-python/'
 class Cluster:
     def __init__(self):
         self.klotz_cluster : Klotz = [Klotz(),Klotz(),Klotz(),Klotz()]
-        self.y = 15
+        self.y = 0
         self.x = 50
         self.form = None
 
-    def setForm(self):
+    def waehleForm(self, waehl_form : Formen):
         y = self.y
         x = self.x
-        z = [(x,y),(x+6,y),(x+6,y+3), (x+(6)*2, y+3)]
+        if waehl_form == Formen.Z:
+            z = [(x,y),(x+3,y),(x+3,y+2), (x+(3)*2, y+2)]
+            self.form = z
+        if waehl_form == Formen.I:
+            i = [(x,y), (x,y+2), (x,(y+2)*2), (x,(y+2)*3)]
+            self.form = i
+        if waehl_form == Formen.L:
+            l = [(x,y), (x,y+2), (x,(y+2)*2), (x+3,(y+2)*2)]
+            self.form = l
+        if waehl_form == Formen.O:
+            o = [(x,y), (x+3, y), (x,y+2), (x+3, y+2)]
+            self.form = o
+        if waehl_form == Formen.T:
+            t = [(x,y), (x-3,y+2), (x,y+2), (x+3, y+2)]
+            self.form = t
+
+    def setForm(self):
         for i in range(len(self.klotz_cluster)):
             k = self.klotz_cluster[i]
-            x, y = z[i]
+            x, y = self.form[i] # !!!
             k.setDrawPosition(y,x)
-        self.form = z
 
     def drawCluster(self, stdscr_fn):
         for i in self.klotz_cluster:
