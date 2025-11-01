44879dc4013931c729e157599ad346eacb1f41f0
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-01 01:10:11 +0100

    Both check-collide-functions are given  list as parameter and improve these functions based on reading and comparing data inside the list.

--- src/tetris_rand.py
+++ src/tetris_rand.py
@@ -18,25 +18,24 @@ class Boden(Tetris_Rand):
     def __init__(self, mul, y, x):
         super().__init__(mul, y, x)
 
-    # Kollidieren: Bitte ausbessern
-    def check_ifCollide_Boden(self, fn_obj_unten): # fn_obj2_pos = z.B. Wand
+    def check_ifCollide_Boden(self, y_pos_u : list):
         collide = False
-        if fn_obj_unten+1 == self.y_pos:
-            collide = True
+        for i in y_pos_u:
+            if i+1 == self.y_pos:
+                collide = True
         return collide
 
 class Wand(Tetris_Rand):
     def __init__(self, mul, y, x):
         super().__init__(mul, y, x)
 
-    def check_ifCollide_Wand(self, fn_obj_seite):
+    def check_ifCollide_Wand(self, x_pos_r_or_l : list):
         collide = False
-        # R / L
-        # Welche Seiten
-        if fn_obj_seite+1 == self.x_pos:
-            collide = True
-        if fn_obj_seite-1 == self.x_pos:
-            collide = True
+        for i in x_pos_r_or_l:
+            if i+1 == self.x_pos:
+                collide = True
+            if i-1 == self.x_pos:
+                collide = True
         return collide
     
     def draw_Rechts(self, stdscr_fn, symbol):  # symbol = z.B.  "|"
