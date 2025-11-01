6483d89f100553d95796fb722cd8cbf3f1cfc069
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-01 01:11:53 +0100

    Make cluster collide with tetris-wall.

--- tetris-game.py
+++ tetris-game.py
@@ -8,6 +8,8 @@ from src.klotz import Cluster
 from src.tetris_rand import Boden
 from src.tetris_rand import Wand
 
+stdscr = curses.initscr()
+
 class Game: pass
 
     
@@ -36,15 +38,14 @@ def main(stdscr):
         boden.draw(stdscr.addstr, '=')
         wand_L.draw(stdscr.addstr, '|\n')
 
-        wand_R.draw_Rechts(stdscr.addstr, '|') # muss anpassen
+        wand_R.draw_Rechts(stdscr.addstr, '|')
 
         # klotz.draw(stdscr) # jede Zeile des Klotzes wird ausgegeben
 
         cluster.setForm()
         cluster.drawCluster(stdscr)
         
-        # stdscr.addstr(10, 10, 'Kollidiert nicht')
-        # stdscr.addstr(8, 10, str(klotz.getUnterseite()))
+        #stdscr.addstr(8, 10, str(cluster.get_Unterseite()))
         #stdscr.refresh()
 
         key = stdscr.getch()
@@ -53,25 +54,15 @@ def main(stdscr):
         if key == curses.KEY_UP and y > 0:
             y -= 1
 
-        # if boden.check_ifCollide_Boden(klotz.getUnterseite()) == False: # klotz ausgetauscht mit cluster
-        #     if key == curses.KEY_DOWN and y < 29: y += 1
+        if boden.check_ifCollide_Boden(cluster.get_Seite('U')) == False:
+            if key == curses.KEY_DOWN: y += 1
 
-        # if wand_R.check_ifCollide_Wand(klotz.get_R_Seite()) == False:
-        #     if key == curses.KEY_RIGHT and x < 118: x += 1
+        if wand_R.check_ifCollide_Wand(cluster.get_Seite('R')) == False:
+            if key == curses.KEY_RIGHT: x += 1
 
-        # if wand_L.check_ifCollide_Wand(klotz.get_L_Seite()) == False:
-        #     if key == curses.KEY_LEFT and x > 0: x -= 1
+        if wand_L.check_ifCollide_Wand(cluster.get_Seite('L')) == False:
+            if key == curses.KEY_LEFT: x -= 1
 
-        
-        if key == curses.KEY_DOWN and y < 29: y += 1
-
-    
-        if key == curses.KEY_RIGHT and x < 118: x += 1
-
-    
-        if key == curses.KEY_LEFT and x > 0: x -= 1
-
-        # klotz.setDrawPosition(y,x)
         cluster.setPos(y,x)
         
 curses.wrapper(main)
