fe1aefebda7401d9828ce1cda8d43943679358e7
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-02 10:24:04 +0100

    Improve get_Seiten() with the option to choose a side of all unmovable clusters. Add the new function kollidiert_seitlich() that checks if a movable object collide with an unmovable cluster.

--- src/cluster_fest.py
+++ src/cluster_fest.py
@@ -8,24 +8,50 @@ class ClusterFest:
         for c in self.unbewegbare_clusters:
             c.drawCluster(fn_stdscr)
 
-    def get_O_Seiten(self):
+    def get_Seiten(self, seite : str):
         oberseiten = []
         for c in self.unbewegbare_clusters:
-            oberseiten += c.get_Seite('O')
+            if seite == 'O':
+                oberseiten += c.get_Seite('O')
+            if seite == 'R':
+                oberseiten += c.get_Seite('R')
+            if seite == 'L':
+                oberseiten += c.get_Seite('L')
+            
         return oberseiten  # = Liste mit aller Oberseiten der Klötze, die in den festsitzenden Cluster vorhanden sind
             # das ist [(y,x), (y,x), ...]
-            
-    def kollidiert_m_Cluster(self, obj_cluster_pos_u : list):
-        kollidiert = False
-        oberseiten = self.get_O_Seiten() # das ist [(y,x), (y,x), ...]
 
-        if len(oberseiten) > 3:pass
+    def kollidiert_oben(self, obj_cluster_pos_u : list):
+        kollidiert = False
+        oberseiten = self.get_Seiten('O') # das ist [(y,x), (y,x), ...]
+
         for pos_u in obj_cluster_pos_u:
             y_u, x_u = pos_u
             for pos_o in oberseiten:
                 y_o, x_o = pos_o
 
                 if y_u+1 == y_o and x_u == x_o:
+                    kollidiert = True
+                    print('Kollidiert')
+        return kollidiert
+    
+    def kollidiert_seitlich(self, cluster_R_or_L : list, get_R_or_L : str): # Wenn Cluster_R -> Fest_L / Fest_R <- Cluster_L
+        kollidiert = False
+        feste_seiten = None
+        if get_R_or_L == 'R':
+            feste_seiten = self.get_Seiten('R') # das ist [(y,x), (y,x), ...]
+        if get_R_or_L == 'L':
+            feste_seiten = self.get_Seiten('L')
+
+        for cluster_pos in cluster_R_or_L:
+            cluster_y, cluster_x = cluster_pos
+            for pos_fest in feste_seiten:
+                y_fest, x_fest = pos_fest
+
+                if cluster_y == y_fest and x_fest == cluster_x-1: # Fest_R <- Cluster_L
+                    kollidiert = True
+                    print('Kollidiert')
+                if cluster_y == y_fest and cluster_x+1 == x_fest: # Cluster_R -> Fest_L
                     kollidiert = True
                     print('Kollidiert')
         return kollidiert
