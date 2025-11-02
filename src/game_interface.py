8b9538e8957ab4c6913f5e4905554bd5fcf50491
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-02 16:38:42 +0100

    Add constructor of Game_Interface and member functions.

--- src/game_interface.py
+++ src/game_interface.py
@@ -1,2 +1,10 @@
 class Game_Interface:
-    def __init__(self): pass
+    def __init__(self):
+        self.punktzahl = 0
+        self.spieler_name = ''
+    
+    def setName(self):
+        self.spieler_name = input('Bitte geben Sie Ihre Spielername ein\n-> ')
+
+    def update_Punktzahl(self, punkte):
+        self.punktzahl = punkte
