b132fcaf75d93758139a11791163f2001f488628
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-02 16:28:22 +0100

    Put every line of code into class TetrisGame, turn local variables into  member variables of TetrisGame and turn  main() into a member function of TetrisGame.

--- tetris-game.py
+++ tetris-game.py
@@ -13,75 +13,77 @@ import random
 random.seed()
 stdscr = curses.initscr()
 
-class TetrisGame: pass
+class TetrisGame:
+    def __init__(self):
+        self.x_start = 37 # Startpunkt
+        self.y_start = 0 # Startpunkt
+        self.screen_width = 80 # screen_width (max 120) original: 119
+        self.screen_height = 30 # screen_height (max 30)
+        self.boden = Boden(self.screen_width, self.screen_height, 0)
+        self.wand_L = Wand(self.screen_height, 0, 0)
+        self.wand_R = Wand(self.screen_height, 0, self.screen_width) # 119
+        self.feste_clusters = ClusterFest()
+        self.cluster = Cluster(self.y_start, self.x_start)
+        self.zufallsform = Formen.T
 
-def main(stdscr):
-    # Bildschirm
-    stdscr.clear()
-    # screen_height, screen_width = stdscr.getmaxyx() // not used
-    x = 39 # Startpunkt
-    y = 0 # Startpunkt
-    screen_width = 80 # screen_width (max 120) original: 119
-    screen_height = 30 # screen_height (max 30)
-    
-    feste_clusters = ClusterFest() # unbewegbare_clusters : Cluster = []
-    boden = Boden(screen_width, screen_height, 0)
-    wand_L = Wand(screen_height, 0, 0)
-    wand_R = Wand(screen_height, 0, screen_width) # 119
-    cluster = Cluster(y, x)
-    zufallsform = Formen.T
-
-    while True:
+    def main(self,stdscr):
+        # Bildschirm
         stdscr.clear()
-        
-        boden.draw(stdscr.addstr, '=')
-        wand_L.draw(stdscr.addstr, '|\n')
-        wand_R.draw_Rechts(stdscr.addstr, '|')
+        # screen_height, screen_width = stdscr.getmaxyx() // not used
+        y, x = self.y_start, self.x_start
+        while True:
+            stdscr.clear()
+            
+            self.boden.draw(stdscr.addstr, '=')
+            self.wand_L.draw(stdscr.addstr, '|\n')
+            self.wand_R.draw_Rechts(stdscr.addstr, '|')
 
-        cluster.waehleForm(Formen(zufallsform))
-        cluster.setForm()
-        cluster.drawCluster(stdscr)
+            self.cluster.waehleForm(Formen(self.zufallsform))
+            self.cluster.setForm()
+            self.cluster.drawCluster(stdscr)
+            # stdscr.addstr(0, 40, 'X')
 
-        feste_clusters.draw(stdscr)
-        
-        # Jeder neu erzeugte Cluster, der dem Variable 'bewegbarer_cluster' zugewiesen bekommt, darf nur durch diese Zuweisung bewegt werden
+            self.feste_clusters.draw(stdscr)
+            
+            # Jeder neu erzeugte Cluster, der dem Variable 'bewegbarer_cluster' zugewiesen bekommt, darf nur durch diese Zuweisung bewegt werden
 
-        isBoden = boden.check_ifCollide_Boden(cluster.get_Seite('U'))
-        isFCluster = feste_clusters.kollidiert_oben(cluster.get_Seite('U'))
-        isFCluster_R = feste_clusters.kollidiert_seitlich(cluster.get_Seite('L'), 'R')  # Wenn Cluster_R -> Fest_L / Fest_R <- Cluster_L
-        isFCluster_L = feste_clusters.kollidiert_seitlich(cluster.get_Seite('R'), 'L')
-        kollidiert = False
-        # Wenn keine Tasten gedrückt werden, dann werden sämtliche Funktionen, die nach key = stdscr.getch() kommen, gar nicht ausgeführt
-        key = stdscr.getch()
-        if key == ord('q'):
-            break
+            isBoden = self.boden.check_ifCollide_Boden(self.cluster.get_Seite('U'))
+            isFCluster = self.feste_clusters.kollidiert_oben(self.cluster.get_Seite('U'))
+            isFCluster_R = self.feste_clusters.kollidiert_seitlich(self.cluster.get_Seite('L'), 'R')  # Wenn Cluster_R -> Fest_L / Fest_R <- Cluster_L
+            isFCluster_L = self.feste_clusters.kollidiert_seitlich(self.cluster.get_Seite('R'), 'L')
+            kollidiert = False
+            # Wenn keine Tasten gedrückt werden, dann werden sämtliche Funktionen, die nach key = stdscr.getch() kommen, gar nicht ausgeführt
+            key = stdscr.getch()
+            if key == ord('q'):
+                break
 
-        if key == curses.KEY_UP and y > 0:
-            y -= 1
-            cluster.setPos(y,x)
-        if key == curses.KEY_DOWN:
-            if isBoden == False and isFCluster == False: # and isFCluster_L == False
-                y += 2
-                cluster.setPos(y,x)
-            else: # Cluster haftet am Boden und kann nicht mehr bewegt werden, der Cluster wird von diesem Variable entfernt 
-                kollidiert = True
-        
-        if wand_R.check_ifCollide_Wand(cluster.get_Seite('R')) == False:
-            if key == curses.KEY_RIGHT:
-                if isFCluster_L == False:
-                    x += 3
-                    cluster.setPos(y,x)
+            if key == curses.KEY_UP and y > 0:
+                y -= 1
+                self.cluster.setPos(y,x)
+            if key == curses.KEY_DOWN:
+                if isBoden == False and isFCluster == False: # and isFCluster_L == False
+                    y += 2
+                    self.cluster.setPos(y,x)
+                else: # Cluster haftet am Boden und kann nicht mehr bewegt werden, der Cluster wird von diesem Variable entfernt 
+                    kollidiert = True
+            
+            if self.wand_R.check_ifCollide_Wand(self.cluster.get_Seite('R')) == False:
+                if key == curses.KEY_RIGHT:
+                    if isFCluster_L == False:
+                        x += 3
+                        self.cluster.setPos(y,x)
 
-        if wand_L.check_ifCollide_Wand(cluster.get_Seite('L')) == False:
-            if key == curses.KEY_LEFT:
-                if isFCluster_R == False:
-                    x -= 3
-                    cluster.setPos(y,x)
+            if self.wand_L.check_ifCollide_Wand(self.cluster.get_Seite('L')) == False:
+                if key == curses.KEY_LEFT:
+                    if isFCluster_R == False:
+                        x -= 3
+                        self.cluster.setPos(y,x)
 
-        if kollidiert == True:
-            feste_clusters.unbewegbare_clusters.append(cluster) # !!!
-            zufallsform = random.randint(1,5)
-            x, y = 50, 0 # draw Koordinaten
-            cluster = Cluster(y,x)
+            if kollidiert == True:
+                self.feste_clusters.unbewegbare_clusters.append(self.cluster) # !!!
+                self.zufallsform = random.randint(1,5)
+                y, x = self.y_start, self.x_start # draw Koordinaten
+                self.cluster = Cluster(y, x)
 
-curses.wrapper(main)
+game = TetrisGame()
+curses.wrapper(game.main)
