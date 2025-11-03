21d55ec3e6209362ca5f24ea260ba1df46f1605f
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-03 17:54:20 +0100

    Commit nothing.

--- src/game_interface.py
+++ src/game_interface.py
@@ -3,8 +3,16 @@ class Game_Interface:
         self.punktzahl = 0
         self.spieler_name = ''
     
-    def setName(self):
-        self.spieler_name = input('Bitte geben Sie Ihre Spielername ein\n-> ')
+    def setName(self): pass#, fn_stdscr pass
+        # wort = ''
+
+        # fn_stdscr.addstr(10, 85, 'Bitte geben Sie Ihre Spielername ein')
+        # while 
+        # buchstabe = input()
+        # return self.spieler_name
+
 
     def update_Punktzahl(self, punkte):
         self.punktzahl = punkte
+
+    def print_Interface(self): pass
