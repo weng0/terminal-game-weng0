9bf57b4c9755fe0406fe47f1d85195af7c1d5381
Cindy <73001331+weng0@users.noreply.github.com>
2025-11-03 18:00:50 +0100

    Add new functions screen_koordinatensystem() that returns a nested list with empty rows. This should recreate the coordinate system of the tetris game.
    Add function kloetze_neu_anordnen() that will realign all blocks with the same y-coordinate one after another in the list. Add delete() will delete the row that is completely filled with blocks."

--- src/cluster_fest.py
+++ src/cluster_fest.py
@@ -1,12 +1,24 @@
 from src.cluster import Cluster
+from src.klotz import Klotz
 
 class ClusterFest:
-    def __init__(self):
+    def __init__(self, screen_height, screen_width):
         self.unbewegbare_clusters : Cluster = []
+        self.screen_height = screen_height
+        self.screen_width = screen_width
+        self.y_schritte = int(screen_height/2)
+        self.x_schritte = int((screen_width-1)/3)
+        self.koordinatensys = None
+        self.alle_kloetze = None
+
+    # def draw(self, fn_stdscr):
+    #     for c in self.unbewegbare_clusters:
+    #         c.drawCluster(fn_stdscr)
 
     def draw(self, fn_stdscr):
-        for c in self.unbewegbare_clusters:
-            c.drawCluster(fn_stdscr)
+        if self.alle_kloetze != None:
+            for k in self.alle_kloetze:
+                k.draw(fn_stdscr)
 
     def get_Seiten(self, seite : str):
         oberseiten = []
@@ -52,3 +64,88 @@ class ClusterFest:
                 if cluster_y == y_fest and cluster_x+1 == x_fest: # Cluster_R -> Fest_L
                     kollidiert = True
         return kollidiert
+    
+    def screen_koordinatensystem(self):
+        # creat screen_blocks
+        screen_blocks = []
+        for ys in range(self.y_schritte):
+            screen_blocks.append([])
+            #for xs in range(self.x_schritte): 
+                #screen_blocks[ys].append('-') # '-' ist hier nur platzhalter bitter ersetzen !!!
+        # print(screen_blocks) funkt
+        return screen_blocks
+    
+    def kloetze_neu_anordnen(self):
+        self.koordinatensys = self.screen_koordinatensystem()
+        alle_kloetze = []
+        for uc in self.unbewegbare_clusters:
+            for k in uc.get_Kloetze():
+                alle_kloetze.append(k)
+        #print(len(alle_kloetze)) funkt
+
+        h = self.screen_height
+        while h > 0:
+            for zeile in self.koordinatensys:
+                #print('h:', h
+                
+                #for elem in range(len(zeile)):
+                w = 1
+                found = True
+                while w < self.screen_width-1:
+                    #print('w:', w)
+                    for k in alle_kloetze:
+                        if h == k.get_y() and w == k.get_x():
+                            #elem = k.get_y(), k.get_x()
+                            zeile.append(k)
+                            found = True
+                            break
+                        else: found = False
+                    if found == False:
+                        zeile.append('-')
+                    w+= 3
+                h -= 2
+                #print('h:', h)
+        if len(alle_kloetze) > 0:pass
+            #print(self.koordinatensys)
+        self.alle_kloetze = alle_kloetze
+
+    def delete(self):
+        verschiebung = 0
+        h = self.screen_height
+        while h > 0:
+            #w = 1
+            
+            #while w < self.screen_width-1:
+            for zeile in self.koordinatensys:
+                align = []
+                for elem in zeile:
+                #print(zeile)
+                    if elem != '-':
+                        if elem.get_y() == h:
+                            align.append(elem)
+                #print(len(align))
+                if len(align) == self.x_schritte:
+                    verschiebung += 1
+                    for a in align:
+                        k = len(self.alle_kloetze)-1
+                        while k > 0:
+                        #for k in len(self.alle_kloetze):
+                            # klotz = self.alle_kloetze[k]
+                            # if klotz.get_y() == a.get_y():
+                            #     del self.alle_kloetze[k]
+                            klotz = self.alle_kloetze[k]
+                            print(k-1)
+                            if klotz.get_y() == a.get_y():
+                                del self.alle_kloetze[k]
+                            k-= 1
+
+                    # k_range = 
+                    # while k_range > 0:
+                    #     if self.alle_kloetze[k_range].get_y() == h:
+                    #         del self.alle_kloetze[k_range]
+                    #         k -= 1
+                h -= 2
+        ## Verschiebung
+        for k in self.alle_kloetze:
+            y = k.get_y() + verschiebung*2
+            k.set_y(y)
